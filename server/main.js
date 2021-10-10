08a638f72f7e71bbd4f4dae6cee686a435fe2a11
tommycroot <tommycroot@rocketmail.com>
2021-10-10 17:16:01 +0100

    initial commit

---
+++ server/main.js
@@ -0,0 +1,34 @@
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+
+Meteor.startup(() => {
+
+});
+
+// let user = {
+//   name: 'Tommy',
+//   location: 'Southend'
+// };
+//
+// let person = {
+//   ...user,
+//   age: 32
+// };
+//
+// console.log(person);
+//
+//
+// let house = {
+//   bedrooms: 2,
+//   bathrooms: 1.5
+// };
+//
+// let yearBuilt = 1995;
+//
+//
+// console.log({
+//   ...house,
+//   bedrooms: 3,
+//   yearBuilt,
+//   flooring: 'carpet'
+// });
